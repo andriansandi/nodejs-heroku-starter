c87c9e2632d607a1b47daedad2f32688b10addb8
Sandi Andrian <andrian.sandi@gmail.com>
2022-02-11 18:05:42 +0700

    add ssloptions

--- config/config.js
+++ config/config.js
@@ -22,6 +22,10 @@ module.exports = {
     "password": DB_PASSWORD,
     "database": DB_DATABASE,
     "host": DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "protocol": 'postgres',
+    "dialectOptions": {
+      ssl: true
+    }
   }
 };
